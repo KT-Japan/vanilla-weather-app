19cf1684fd0aeb4ff6480fc53305de8ebd05c96e
KT-Japan <95172229+KT-Japan@users.noreply.github.com>
2022-01-30 12:08:00 +0900

    Made the img responsive

--- src/script.js
+++ src/script.js
@@ -29,18 +29,26 @@ function displayTemperature(response){
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
-    
+    let iconElement = document.querySelector("#icon");
+
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     conditionsElement.innerHTML = response.data.weather[0].main;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = formateDate(response.data.dt * 1000);
+    iconElement.setAttribute("src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute(
+        "alt",
+        response.data.weather[0].description);
+    
 }
 
 let apiKey ="7e5b42f15f07c7e23193e5065b6e4cf9";
+let city = "London";
 let apiUrl =`https://api.openweathermap.org/data/2.5/
-weather?q=London
+weather?q=${city}
 &appid=${apiKey}
 &units=metric`;
 axios.get(apiUrl).then(displayTemperature);
